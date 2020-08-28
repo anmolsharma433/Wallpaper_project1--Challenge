fa3d195ca7f67c70e6a0f125f5d0a3f50e52c7dd
Anmol Sharma <anmolsharma433@gmail.com>
2020-08-27 23:05:43 -0400

    Initial Commit

---
+++ Wallpaper_project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wallpaper_project1
+//
+//  Created by Anmol Sharma on 2020-08-27.
+//  Copyright © 2020 Anmol Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
